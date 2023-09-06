41f5eaab67d93fe9923a245461478a91b56509d7
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 01:54:19 +0300

    implement the first algorithm, the one with O(N^3) time complexity

--- chapter_2/maximum_subarray_sum.cpp
+++ chapter_2/maximum_subarray_sum.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+using namespace std;
+
+int arr [8] = {-1, 2, 4, -3, 5, 2, -5, 2 };
+int n = 8;
 
 int algorithm_1() {
     int best = 0;
+    for (int a = 0; a < n; a++) {
+        for (int b = a; b < n; b++) {
+            int sum = 0;
+            for (int k = a; k <= b; k++) {
+                sum += arr[k];
+            }
+            best = max(best, sum);
+        }
+    }
+    return best;
 }
 
 int main() {
-
+    int best = algorithm_1();
+    cout << best << "\n";
 }
