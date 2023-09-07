db04aaf57fea48a84b1445fc19e83a7d39a93f5c
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 19:01:36 +0300

    set best variable to `INT_MIN`

--- chapter_2/maximum_subarray_sum.cpp
+++ chapter_2/maximum_subarray_sum.cpp
@@ -5,7 +5,7 @@ int arr [8] = { -1, 2, 4, -3, 5, 2, -5, 2 };
 int n = 8;
 
 int algorithm_1() {
-    int best = 0;
+    int best = INT_MIN;
     for (int a = 0; a < n; a++) {
         for (int b = a; b < n; b++) {
             int sum = 0;
@@ -19,7 +19,7 @@ int algorithm_1() {
 }
 
 int algorithm_2() {
-    int best = 0;
+    int best = INT_MIN;
     for (int a = 0; a < n; a++) {
         int sum = 0;
         for (int b = a; b < n; b++) {
@@ -31,7 +31,7 @@ int algorithm_2() {
 }
 
 int algorithm_3() {
-    int best = 0, sum = 0;
+    int best = INT_MIN, sum = 0;
     for (int k = 0; k < n; k++) {
         sum = max(arr[k], sum + arr[k]);
         best = max(best, sum);
