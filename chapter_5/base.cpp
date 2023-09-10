2e88cd3b0a7f977909bdd89c8a9aaeeaaf3cf684
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-10 19:47:44 +0300

    remove namespace

--- chapter_5/base.cpp
+++ chapter_5/base.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include <vector>
-#include "base.hpp"
-using namespace Basis;
 
-namespace Base {
-    template <typename T> class BaseClass: public BasisClass<T> {
-        protected:
-            int length = 0;
-        public:
-            std::vector<T> vec;
-            void increment_length() {
-                length += 1;
-            }
-            void decrement_length() {
-                length -= 1;
-            }
-            void add_element(T K) {
-                vec.push_back(K);
-                increment_length();
-            }
+template <class T> class BaseClass {
+    protected:
+        int length = 0;
+    public:
+        std::vector<T> vec;
+        void increment_length() {
+            length += 1;
+        }
+        void decrement_length() {
+            length -= 1;
+        }
+        void add_element(T K) {
+            vec.push_back(K);
+            increment_length();
+        }
 
-            void remove_element(T K) {
-                vec.pop_back(K);
-                decrement_length();
-            }
+        void remove_element(T K) {
+            vec.pop_back(K);
+            decrement_length();
+        }
 
-            int get_vec_length() {
-                return length;
+        int get_vec_length() {
+            return length;
+        }
+
+        void print_elements() {
+            std::cout << "Elements in vector: " << std::endl;
+            for (T c: vec) {
+                std::cout << c << ' ';
             }
-    };
-}
+            std::cout << std::endl;
+        }
+};
+
